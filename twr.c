c976c05a37121f894d2e6cff834b3c58189a90ed
funksh0n <funksh0n@protonmail.ch>
2019-02-16 11:52:38 +0000

    Cast args to double

--- twr.c
+++ twr.c
@@ -1,17 +1,20 @@
 /* See LICENCE for licence details. */
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main(int argc, char *argv[])
 {
 	double thrust, mass, acceleration, force, ratio;
-	thrust = argv[1];
-	mass = argv[2];
-	acceleration = argv[3];
+	thrust = atof(argv[1]);
+	mass = atof(argv[2]);
+	acceleration = atof(argv[3]);
 	force = mass * acceleration;
 	ratio = thrust / force;
 
-	printf("m = %dkg\na = %dm/s^2\nW = m * a = %dN\n", mass, acceleration, force);
-	printf("T = %dkN\nT / W = %dkN/N\n", thrust, ratio);
-	printf("TWR = %d", ratio * 1000);
+	printf("m = %fkg\na = %fm/s^2\nW = m * a = %fN\n", mass, acceleration, force);
+	printf("T = %fkN\nT / W = %fkN/N\n", thrust, ratio);
+	printf("TWR = %f\n", ratio * 1000);
+
+	return 0;
 }
